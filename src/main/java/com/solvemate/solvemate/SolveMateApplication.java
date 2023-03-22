1f46b1bda55e13ce026a7d15560490e81285f305
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-03-22 17:25:17 +0900

    Initial commit

---
+++ src/main/java/com/solvemate/solvemate/SolveMateApplication.java
@@ -0,0 +1,13 @@
+package com.solvemate.solvemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolveMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolveMateApplication.class, args);
+	}
+
+}
